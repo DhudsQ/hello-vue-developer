d659f533491c1bc5e6a424bff01066a16984ceb9
Tu Nombre <tu-email@example.com>
2026-04-17 18:18:59 -0500

    feat(developer-registration): add developer

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import DeveloperRegistration from "./greetings/presentation/components/developer-registration.vue";
+import {ref} from "vue";
+
+const firstName = ref('');
+const lastName = ref('');
+const developerCount=ref(0)
+const hastRegistered=ref(false)
+
+function updateRegisteredDeveloperInfo(developer){
+  firstName.value=developer.firstName;
+  lastName.value=developer.lastName;
+  hastRegistered.value=true;
+  console.log('Developer registered: ' ,  developer);
+
+}
+
+function resetRegisteredDeveloperInfo() {
+  firstName.value = '';
+  lastName.value = '';
+  hastRegistered.value = false;
+  console.log('Registration deferred');
+}
+
+function updateDeveloperInfo(developer){
+  const dev=new Developer(developer.firstName,developer.lastName);
+  if(dev.fullName !== 'Unknown')
+    developerCount.value++;
+}
 </script>
 
 <template>
-  <HelloWorld />
+  <developer-registration
+    @developer-registered="updateRegisteredDeveloperInfo"
+    @registered-deferred="resetRegisteredDeveloperInfo"
+    />
 </template>
